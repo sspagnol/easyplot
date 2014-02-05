3dfc3cbbe49dc29546275486e678aac590121b6d
s.spagnol@aims.gov.au <s.spagnol@aims.gov.au>
2014-02-05 09:52:11 +0000

    Tidy up plotting routines

--- plotWS.m
+++ plotWS.m
@@ -7,39 +7,49 @@ varList=whos;
 % selVarInd = sscanf(selVarInd, '%d');
 selVarInd=1:length(varList);
 
-%set plot figure and toolbar
-hFigure=figure('Visible','off','ToolBar','figure');
+%Create a string for legend
+legendStr={};
+
+mp = get(0, 'MonitorPositions');
+screen_size = mp(1,:);
+screen_size = [0 0 mp(1,3) mp(1,4) ] .* 0.80 + 50;
 %create a toolbar and a toggle button to display or not the legend
 %toggleLegend(hFigure);
+%fh_overlay=figure('Position',screen_size, 'Visible','off');
+fh_overlay=figure('Position',screen_size,'Visible','off','ToolBar','figure');
+figure(fh_overlay);
+%set(fh_overlay, 'Visible', 'off');
+set(fh_overlay,'Color',[1 1 1]);
+hold('on');
 
-%Create a string for the plot and legend
-plotStr=[];
-legendStr=[];
+ii=1;
 for i=1:length(selVarInd)
-    
     %only plot if variables is a time serie... ie has two colunm and his double,
     if varList(selVarInd(i)).size(2)==2 &&  strcmp(varList(selVarInd(i)).class,'double');
-        plotStr=[plotStr varList(selVarInd(i)).name '(:,1),' varList(selVarInd(i)).name '(:,2),' ];
-        legendStr=[legendStr '''' varList(selVarInd(i)).name '''' ',' ];
+        theData=eval(varList(selVarInd(i)).name);
+        plot(theData(:,1), theData(:,2));
+        legendStr{ii}=strrep(varList(selVarInd(i)).name, '_', '\_');
+        ii=ii+1;
     end
 end
 
-%finishing off plotStr syntax
-plotStr=['plot(' plotStr(1:end-1) ')'];
-%evaluate plotStr
-eval(plotStr);
+h = findobj(gca,'Type','line');
+mapping = round(linspace(1,64,length(h)))';
+colors = colormap('jet');
+for j = 1:length(h)
+    try
+        set(h(j),'Color',colors( mapping(j),: ));
+    catch e
+        fprintf('Error changing plot colours in plot %s \n',get(gcf,'Name'));
+        disp(e.message);
+    end
+end
 
-%finishing off legendStr syntax
-legendStr=['hLegend=legend(' legendStr(1:end-1) ');'];
-%evaluate legendStr
-eval(legendStr)
-
-datetick('x','dd-mmm-yyyy')
-xlabel('Time (UTC)')
-setDate4zoom
-set(hFigure,'Visible','on');
-set(hLegend,'Interpreter','none');
-legend('off')
-
+datetick('x','dd-mmm-yyyy');
+xlabel('Time (UTC)');
+setDate4zoom;
+set(fh_overlay,'Visible','on');
+%set(hLegend,'Interpreter','none');
+legend(legendStr);
 
 clear hfigure i selVarInd plotStr legendStr varList hLegend
