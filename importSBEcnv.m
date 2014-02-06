8bcd8a04444dc7a71d164c7e7ca1a24cf126af21
s.spagnol@aims.gov.au <s.spagnol@aims.gov.au>
2014-02-06 11:55:04 +0000

    Clean up sample_data updating

--- importSBEcnv.m
+++ importSBEcnv.m
@@ -5,21 +5,15 @@ if ischar(FILENAME)
     FILENAME = {FILENAME};
 end
 
-iend=0;
-if exist('sample_data','var')
-    iend=numel(sample_data);
+if ~exist('sample_data','var')
+    sample_data={};
 end
 
-notLoaded=0;
 for ii=1:length(FILENAME)
-    if exist('sample_data','var')
-        notLoaded = ~any(cell2mat((cellfun(@(x) ~isempty(strfind(x.toolbox_input_file, char(FILENAME{ii}))), sample_data, 'UniformOutput', false))));
-    else
-        notLoaded = 1;
-    end
+    notLoaded = ~any(cell2mat((cellfun(@(x) ~isempty(strfind(x.toolbox_input_file, char(FILENAME{ii}))), sample_data, 'UniformOutput', false))));
     if notLoaded
         disp(['importing file ', num2str(ii), ' of ', num2str(length(FILENAME)), ' : ', char(FILENAME{ii})]);
-        sample_data{ii+iend} = SBE19Parse( {fullfile(PATHNAME,FILENAME{ii})}, 'timeseries' );
+        sample_data{end+1} = SBE19Parse( {fullfile(PATHNAME,FILENAME{ii})}, 'timeseries' );
     else
         disp(['File ' char(FILENAME{ii}) ' already loaded.']);
     end
