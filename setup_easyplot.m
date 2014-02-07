89d8db77647c9506b33073c39947eba598741798
s.spagnol@aims.gov.au <s.spagnol@aims.gov.au>
2014-02-07 00:29:45 +0000

    streamline instrumnent import, add rdi, only allows 1d data vectors to plotted

---
+++ setup_easyplot.m
@@ -0,0 +1,39 @@
+function setup_easyplot
+
+% path to easyplot dir
+easyplotDir='C:\prj\GitLab\easyplot';
+
+% location of the users toolbox installation
+imos_tb_home='C:\AIMS\imos-toolbox-2.3b-sbs';
+
+% user should not need to edit anything further
+
+disp('Adding easyplot, please wait ...');
+disp(['Easyplot path : ' easyplotDir]);
+gp=genpath_clean(easyplotDir);
+addpath(gp);
+
+% add all folders and subfolders in imos_tb_home to the path
+disp('Adding IMOS-toolbox, please wait ...');
+disp(['IMOS-toolbox path : ' imos_tb_home]);
+gp=genpath_clean(imos_tb_home);
+addpath(gp);
+
+end
+
+%%
+function thePath = genpath_clean( topDir )
+%genpath_clean generate a path without .svn, .git etc.
+%   generate a path without .svn, .git etc. Based on idea from OpenEarthTools
+
+b=genpath(topDir);
+s = strread(b, '%s','delimiter', pathsep);  % read path as cell
+rpattern='(\.svn|\.git|\.hg)';
+ii=cellfun(@isempty,regexp(s,rpattern,'match','once'));
+s=s(ii); %cell array without .git etc
+thePath=sprintf(['%s' pathsep],s{:}); %make string seperated by pathsep
+if thePath(end)==pathsep % remove last not needed pathsep
+    thePath(end)=[];
+end
+
+end
