fa43e5c8bd12f5ff82396e5d996b0773c42ed4d8
sspagnol <s.spagnol@aims.gov.au>
2016-10-31 11:11:03 +0800

    Move matlab build files to utilities folder.

--- snapshot/buildBinaries.py
+++ snapshot/buildBinaries.py
@@ -6,9 +6,9 @@ import time
 import shutil
 
 # buildBinaries.py
-# Exports the IMOS Toolbox from SVN and
+# Exports easyplot from AIMS gitlab and
 #
-#   - Runs Util/imosCompile.m to create a ddb.jar and imosToolbox executables
+#   - Runs Util/imosCompile.m to create an easyplot executable
 # 
 # Both of these files are copied to the relevant directory and commited to SVN.
 #
@@ -17,10 +17,11 @@ import shutil
 #
 
 lt = time.localtime()
-#% http://gitlab.aims.gov.au/ogtech/easyplot.git
+# http://gitlab.aims.gov.au/ogtech/easyplot.git
 project = 'easyplot'
 
-version    = 'master'
+# version    = 'master'
+version    = 'main_function_cleanup'
 
 url        = ' http://gitlab.aims.gov.au/ogtech/%s.git' % project
 exportDir  = 'export'
@@ -28,34 +29,35 @@ exportDir  = 'export'
 compilerLog = '.\%s\log.txt' % exportDir
 
 #
-# export from SVN
+# clone from git
 #
 print('\n--exporting tree from %s to %s' % (url, exportDir))
 os.system('git clone %s %s' % (url, exportDir))
 os.system('cd %s && git checkout %s' % (exportDir, version))
+os.system('git status')
 
-#
+
 # remove snapshot directory
 #
 print('\n--removing snapshot')
 shutil.rmtree('%s/snapshot' % exportDir)
 
-#
+
 # build DDB interface
 #
-print('\n--building DDB interface')
-compiled = os.system('cd %s/Java && ant install' % exportDir)
+#print('\n--building DDB interface')
+#compiled = os.system('cd %s/Java && ant install' % exportDir)
 
-if compiled is not 0:
-  print('\n--DDB interface compilation failed - cleaning')
-  os.system('cd %s/Java && ant clean' % exportDir)
+# if compiled is not 0:
+#  print('\n--DDB interface compilation failed - cleaning')
+#  os.system('cd %s/Java && ant clean' % exportDir)
 
-#
+
 # create snapshot
 #
 print('\n--building Matlab binaries')
 matlabOpts = '-nodisplay -nojvm -wait -logfile "%s"' % compilerLog
-matlabCmd = "addpath('snapshot'); try, easyplotCompile(); exit(); catch e, disp(e.message); end;"
+matlabCmd = "addpath('utilities'); try, easyplotCompile(); exit(); catch e, disp(e.message); end;"
 os.system('cd %s && matlab %s -r "%s"' % (exportDir, matlabOpts, matlabCmd))
 
 print('\n--removing local git tree')
