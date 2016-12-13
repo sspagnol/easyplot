d36f7bf4443882d82db5087bf9289acb4a87c4bc
sspagnol <s.spagnol@aims.gov.au>
2016-12-13 12:57:18 +0800

    Inelegant workaround to handle if a user opens an '_ep.fig' created by a different user. EPdir may be different.

--- private/exit_Callback.m
+++ private/exit_Callback.m
@@ -7,7 +7,13 @@ function exit_Callback(hObject, eventdata, oldHandles)
 % handles    structure with handles and user data (see GUIDATA)
 userData=getappdata(ancestor(hObject,'figure'), 'UserData');
 gData = guidata(ancestor(hObject,'figure'));
+
 userData.ini.startDialog.dataDir=userData.oldPathname;
+
+% inelegant code to handle if user double clicked on a '_ep.fig' and stored
+% EPdir is different to current.
+[tmpEPdir, ~, ~] = fileparts(which('easyplot'));
+userData.EPdir = tmpEPdir;
 struct2ini(fullfile(userData.EPdir,'easyplot.ini'),userData.ini);
 
 delete(gData.figure1);
