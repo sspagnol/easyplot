bf6c40b139282b81100de7b775bc23def592a209
sspagnol <s.spagnol@aims.gov.au>
2017-10-18 15:46:32 +0800

    Shorten 'Longranger RDI' to 'RDI' for legend strings

--- functions/workhorseParseCleanup.m
+++ functions/workhorseParseCleanup.m
@@ -4,6 +4,7 @@ function [ sam ] = workhorseParseCleanup( sam )
 % make instrument_model a little shorter
 sam.meta.instrument_model = strtrim(strrep(sam.meta.instrument_model, 'Sentinel or Monitor', ''));
 sam.meta.instrument_model = strtrim(strrep(sam.meta.instrument_model, 'Workhorse ADCP', 'RDI'));
+sam.meta.instrument_model = strtrim(strrep(sam.meta.instrument_model, 'Long Ranger', ''));
 
 end
 
