bb34539a5a38295fcf25646bb274fb726b53c1c3
sspagnol <sspagnol@aims.gov.au>
2018-10-24 11:28:08 +1000

    Make instrument short names for netcdf import.

---
+++ functions/netcdfParseCleanup.m
@@ -0,0 +1,29 @@
+function [ sam ] = netcdfParseCleanup( sam )
+%netcdfParseCleanup Easyplot struct cleanup
+
+% make instrument_model string a little shorter for display purposes
+
+instrument_model = {'Workhorse ADCP', ...
+    'ADCP - WORKHORSE SENTINEL NEMO', ...
+    'SBE16PLUS V2 SEACAT', ...
+    'TEMPERATURE RECORDER', ...
+    'TEMPERATURE LOGGER SOLO T'};
+
+instrument_model_shortname = {'RDI', ...
+    'RDI NEMO', ...
+    'SBE16PLUS V2', ...
+    'TEMP', ...
+    'SOLO T'};
+
+for ii = 1:length(instrument_model)
+    if ~isempty(regexp(sam.meta.instrument_model, instrument_model{ii}, 'once'))
+        sam.meta.instrument_model_shortname  = regexprep(sam.meta.instrument_model, instrument_model{ii}, instrument_model_shortname{ii});
+    end
+end
+                           
+if isempty(sam.meta.instrument_serial_no)
+    sam.meta.instrument_serial_no ='?';
+end
+
+end
+
