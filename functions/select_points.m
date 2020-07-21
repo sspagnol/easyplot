b990dda3fc44ae21aedf7e82c98222c5138193ee
Simon Spagnol <s.spagnol@mail.com>
2020-07-21 10:29:58 +0800

    Update select_points.m to use drawrectangle if Matlab > R2018b.

--- functions/select_points.m
+++ functions/select_points.m
@@ -1,4 +1,4 @@
-function [x,y,ph] = select_points(hAx)
+function [x, y, recH] = select_points(hAx)
 % SELECT_POINTS uses rbbox to select points in the timeseries chart for flagging.
 %
 % [x,y,ph] = select_points(hAx)
@@ -8,19 +8,25 @@ function [x,y,ph] = select_points(hAx)
 %   x,y : index of rectangle corners in figure units 
 %   ph : plot handle for box
 
-axes(hAx);
-k = waitforbuttonpress;
-point1 = get(gca,'CurrentPoint');    % button down detected
-finalRect = rbbox;                   % return figure units
-point2 = get(gca,'CurrentPoint');    % button up detected
-point1 = point1(1,1:2);              % extract x and y
-point2 = point2(1,1:2);
-p1 = min(point1,point2);             % calculate locations
-offset = abs(point1-point2);         % and dimensions
-x = [p1(1) p1(1)+offset(1) p1(1)+offset(1) p1(1) p1(1)];
-y = [p1(2) p1(2) p1(2)+offset(2) p1(2)+offset(2) p1(2)];
-hold('on');
-axis('manual');
-ph = plot(x,y);                            % redraw in dataspace units
+if verLessThan('matlab', '9.5.0')
+    axes(hAx);
+    k = waitforbuttonpress;
+    point1 = get(gca,'CurrentPoint');    % button down detected
+    finalRect = rbbox;                   % return figure units
+    point2 = get(gca,'CurrentPoint');    % button up detected
+    point1 = point1(1,1:2);              % extract x and y
+    point2 = point2(1,1:2);
+    p1 = min(point1,point2);             % calculate locations
+    offset = abs(point1-point2);         % and dimensions
+    x = [p1(1) p1(1)+offset(1) p1(1)+offset(1) p1(1) p1(1)];
+    y = [p1(2) p1(2) p1(2)+offset(2) p1(2)+offset(2) p1(2)];
+    hold('on');
+    axis('manual');
+    recH = plot(x,y);                            % redraw in dataspace units
+else
+    recH = drawrectangle(hAx, 'Rotatable', false);
+    x = recH.Vertices(:,1);
+    y = recH.Vertices(:,2);
+end    
 
 end
