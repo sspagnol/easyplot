5c0310f373e01906e661170f622a18e81bc25977
Simon Spagnol <s.spagnol@aims.gov.au>
2022-12-19 10:07:29 +0800

    Logic change to shorten long instrument names.

--- functions/netcdfParseCleanup.m
+++ functions/netcdfParseCleanup.m
@@ -3,14 +3,19 @@ function [ sam ] = netcdfParseCleanup( sam )
 
 % make instrument_model string a little shorter for display purposes
 
-[instrument_model, EP_instrument_model_shortname] = EPshortnameMap();
-
-for ii = 1:length(instrument_model)
-    if ~isempty(regexp(sam.meta.instrument_model, instrument_model{ii}, 'once'))
-        sam.meta.EP_instrument_model_shortname  = regexprep(sam.meta.instrument_model, instrument_model{ii}, EP_instrument_model_shortname{ii});
-    end
+shortname_map = EPshortnameMap();
+names = shortname_map.keys;
+values = shortname_map.values;
+   
+sam_desc = genSampleDataDesc(sam, 'medium');
+sam_desc = strtrim(sam_desc(1:strfind(sam_desc,'(')-1));
+tf = ~cellfun(@isempty, regexpi(sam_desc, names));
+if any(tf)
+   ind = find(tf);
+   sam_desc = strrep(sam_desc, names{ind}, values{ind});
 end
-                           
+sam.meta.EP_instrument_model_shortname  = sam_desc;
+
 if isempty(sam.meta.instrument_serial_no)
     sam.meta.instrument_serial_no ='?';
 end
