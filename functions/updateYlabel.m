2c6bc12127a19e4138bf6f27a4027a280ae804ae
Simon Spagnol <s.spagnol@aims.gov.au>
2024-02-14 15:28:14 +1000

    Add imosParameter.txt like list for easyplot variables so have units available for plotting purposes.

--- functions/updateYlabel.m
+++ functions/updateYlabel.m
@@ -6,11 +6,21 @@ short_name = hAx.Tag;
 if strcmp(short_name, 'MULTI')
     ylabelStr = 'Multiple Variables';
 else
-    long_name = imosParameters( short_name, 'long_name' );
-    try      uom = ['(' imosParameters(short_name, 'uom') ')'];
-    catch e, uom = '';
+    [long_name, is_ep_param] = easyplotParameters( short_name, 'long_name' );
+    if is_ep_param
+        uom = ['(' easyplotParameters(short_name, 'uom') ')'];
+        ylabelStr = makeYlabel( short_name, short_name, uom );
+    else
+        long_name = imosParameters( short_name, 'long_name' );
+        try
+            uom = ['(' imosParameters(short_name, 'uom') ')'];
+        catch e
+            uom = '';
+        end
+        ylabelStr = makeYlabel( short_name, long_name, uom );
     end
-    ylabelStr = makeYlabel( short_name, long_name, uom );
+    
+    
 end
 
 ylabel(hAx, ylabelStr, 'Interpreter', 'none');
